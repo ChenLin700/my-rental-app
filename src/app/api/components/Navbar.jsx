5816e8f5a3ca3c775743f8140544e6cc1d4209f2
ChenLin700 <st970366@gmail.com>
2025-07-19 16:23:39 +0800

    新增修改

--- src/app/api/components/Navbar.jsx
+++ src/app/api/components/Navbar.jsx
@@ -13,10 +13,10 @@ export default function Navbar() {
             房源列表
           </Link>
           <Link href="/news" className="text-gray-700 hover:text-blue-600">
-            修改1
+            最新消息224
           </Link>
           <button className="px-4 py-1 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-            登入
+            登入224
           </button>
         </div>
       </div>
